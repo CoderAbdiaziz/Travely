5208c8e202d97e4fa1865ec2678b1214f786c786
Abdiaziz Mohamoud <moha0469@d.umn.edu>
2020-07-27 23:05:05 -0500

    map
    Fully displayed working map on the map fragment. Fixed issue where app would crash when switching on and then back too the map fragment.

--- app/src/main/java/Fragments/MapFragment.java
+++ app/src/main/java/Fragments/MapFragment.java
@@ -10,12 +10,17 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.travely.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
 import static com.parse.Parse.getApplicationContext;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(
@@ -23,17 +28,24 @@ public class MapFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Initialize the SDK
-        Places.initialize(getApplicationContext(), "AIzaSyBaJ3CFZqfgbU804rsS_RD73xuLnPEzFc8");
+    }
 
-        // Create a new PlacesClient instance
-        PlacesClient placesClient = Places.createClient(getContext());
-
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
